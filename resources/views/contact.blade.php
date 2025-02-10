2974e5f3e35229c6375968bb718ecd9fbe2bdcf2
Rayan Encarnacion <rayanbalbuena@gmail.com>
2025-02-10 19:51:31 -0400

    Day 4: Learning about components attributes and named slots

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,3 +1,4 @@
 <x-layout>
+    <x-slot:heading>Contact page</x-slot:heading>
     <h1>Contact page is here!!!</h1>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('/');
 
 Route::get('/about', function () {
     return view('about');
-});
+})->name('/about');
 
 Route::get('/contact', function () {
     return view('contact');
-});
+})->name('/contact');
 

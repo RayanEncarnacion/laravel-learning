dcc7908ca4fc52b04150626ecd650b381ee1c64c
Rayan Encarnacion <rayanbalbuena@gmail.com>
2025-02-14 21:12:29 -0400

    feat: Relate jobs to employer

---
+++ database/migrations/2025_02_14_122154_create_posts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\Employer;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->text('text');
+            $table->foreignIdFor(Employer::class);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            Schema::dropIfExists('posts');
+        });
+    }
+};

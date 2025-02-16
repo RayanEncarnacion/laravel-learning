ff17a5c1aa2d1bae9817725555f4b75000d13e75
Rayan Encarnacion <rayanbalbuena@gmail.com>
2025-02-15 20:08:56 -0400

    feat: Add link to job form

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -1,6 +1,11 @@
 <x-layout>
     <x-slot:title>Jobs</x-slot:title>
-    <x-slot:heading>Jobs Listing</x-slot:heading>
+    <x-slot:heading>
+        <div class="flex items-center justify-between">
+            <span>Jobs Listing</span>
+            <a href="/jobs/create" class="pointer-events-auto rounded-md px-3 py-2 text-[0.8125rem]/5 font-semibold text-gray-500 border shadow-sm border-gray-200 hover:bg-[#fcfcfc]">Create Job</a>
+        </div>
+    </x-slot:heading>
 
     <div class="space-y-3 mb-3">
         @foreach ($jobs as $job)

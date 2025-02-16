03f32935568c55db050000deb55497e001af48b5
Rayan Encarnacion <rayanbalbuena@gmail.com>
2025-02-15 20:12:19 -0400

    chore: Updated form styles

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -6,11 +6,11 @@
       @csrf
 
         <div class="space-y-12">
-            <div class="border-b border-gray-900/10 pb-12">
+            <div class="pb-4">
               <h2 class="text-base/7 font-semibold text-gray-900">Create a new job</h2>
               <p class="mt-1 text-sm/6 text-gray-600">We need a few key details to create a job.</p>
         
-              <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+              <div class="mt-6 grid grid-cols-1 gap-4 sm:grid-cols-6">
                 <div class="sm:col-span-4">
                   <label for="title" class="block text-sm/6 font-medium text-gray-900">Title</label>
                   <div class="mt-2">
@@ -53,8 +53,7 @@
             </div>
         </div>
       
-        <div class="mt-6 flex items-center gap-x-6">
-          <button type="button" class="text-sm/6 font-semibold text-gray-900">Cancel</button>
+        <div class="mt-4 flex items-center gap-x-6">
           <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
         </div>
       </form>

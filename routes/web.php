febb17f5de1b3b4ef6af5f8abadac2356511eba6
Rayan Encarnacion <rayanbalbuena@gmail.com>
2025-02-17 15:45:16 -0400

    refactor: Add route resource to streamline jobs routes

--- routes/web.php
+++ routes/web.php
@@ -7,12 +7,4 @@ Route::view("/", 'welcome');
 Route::view("/about", 'about');
 Route::view("/contact", 'contact');
 
-Route::controller(JobController::class)->group(function () {
-    Route::get('/jobs', 'index')->name('jobs');
-    Route::get('/jobs/create', 'create')->name('jobs');
-    Route::post('/jobs', 'store')->name('jobs');
-    Route::get('/jobs/{job}', 'show');
-    Route::get('/jobs/{job}/edit', 'edit');
-    Route::patch('/jobs/{job}', 'update');
-    Route::delete('/jobs/{job}', 'destroy');
-});
+Route::resource('jobs', JobController::class);

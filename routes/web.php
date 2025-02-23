202e3fe0858a7a28639d72bf594593a84ef760fc
Rayan Encarnacion <rayanbalbuena@gmail.com>
2025-02-22 22:43:25 -0400

    chore: Remove unused translateJob route

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,7 @@
 <?php
 
 use App\Http\Controllers\JobController;
-use App\Jobs\TranslateJob;
-use App\Models\Job;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/translateJob', function () {
-    $job = Job::first();
-    TranslateJob::dispatch($job);
-
-    return 'Done';
-});
 
 Route::view("/", 'welcome');
 Route::view("/about", 'about');
